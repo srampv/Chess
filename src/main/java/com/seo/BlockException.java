ab5b53b98257ef9df0d2e5c71c75c30175159cc5
venkata <srampv@gmail.com>
2022-02-06 19:13:03 -0500

    Update BlockException.java

--- src/main/java/com/seo/BlockException.java
+++ src/main/java/com/seo/BlockException.java
@@ -2,7 +2,7 @@ package com.seo;
 
 public class BlockException extends RuntimeException
 {
-  //exception for block
+  //exception for each piece block  exception
   public BlockException(String msg)
   {
     super(msg);
